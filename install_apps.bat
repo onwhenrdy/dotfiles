2f4bdbb892889369ca3317586f4835422685f6a8
Dominik Selzer <dominik.selzer@uni-saarland.de>
2024-07-26 20:42:32 +0200

    Add a more robust install script

--- install_apps.bat
+++ install_apps.bat
@@ -1,29 +1,25 @@
-scoop bucket add extras
-scoop bucket add main
-
-scoop install git
-scoop install extras/gh
-scoop install just
-scoop install main/watchexec@2.0.0
-scoop install lazygit
-scoop install neovim
-scoop install micro
-sccop install extras/vscode
-scoop install main/starship
-scoop install fzf
-scoop install zoxide
-scoop install ntop
-sccop install perl
-scoop install extras/wezterm
-scoop install ripgrep
-scoop install broot
-scoop install lsd
-scoop install bat
-scoop install dua
-scoop install unar jq poppler fd yazi
-
+scoop bucket add extras & ^
+scoop bucket add main & ^
+scoop install git & ^
+scoop install extras/gh & ^
+scoop install just & ^
+scoop install main/watchexec@2.0.0 & ^
+scoop install lazygit &  ^
+scoop install neovim & ^
+scoop install micro & ^
+scoop install extras/vscode & ^
+scoop install main/starship & ^
+scoop install fzf & ^
+scoop install zoxide & ^
+scoop install ntop & ^
+scoop install perl & ^
+scoop install extras/wezterm & ^
+scoop install ripgrep & ^
+scoop install broot & ^
+scoop install lsd & ^
+scoop install bat & ^
+scoop install dua & ^
+scoop install unar jq poppler fd yazi & ^
+perl -MCPAN -e"install 'File::HomeDir'" & ^
+perl -MCPAN -e"install 'YAML::Tiny'" & ^
 iwr -useb https://raw.githubusercontent.com/sigoden/window-switcher/main/install.ps1 | iex
-
-pip install radian
-perl -MCPAN -e"install 'File::HomeDir'"
-perl -MCPAN -e"install 'YAML::Tiny'"
